a5303069cf7c2bcaba299e27d3be212b80089a00
xiabaoying <xiabaoying@zhihu.com>
2021-05-17 16:50:38 +0800

    feat: POST 表单攻击

--- server/server.js
+++ server/server.js
@@ -22,25 +22,28 @@ router.all('/api/login', ctx => {
 
 router.all('/api/appinfo', ctx => {
     const { id } = getReqData(ctx)
+    console.log(`当前时间 ${Date.now()}: debug 的数据是 id: `, id)
     if (db[id]) {
-        ctx.body = {
-            errno: 0,
-            data: {
-                money: db[id],
-                userName: id
-            }
+      ctx.body = {
+        errno: 0,
+        data: {
+          money: db[id],
+          userName: id
         }
+      }
     } else {
-        ctx.body = { errno: 666 }
+      ctx.body = { errno: 666 }
     }
-})
+  })
 
-router.all('/api/transfer', ctx => {
-    const { query: { toUser, money }, id } = getReqData(ctx)
+router.post('/api/transfer', ctx => {
+    const { body: { toUser, money }, id } = getReqData(ctx)
     if (!id) {
         ctx.body = { errno: 666, errmsg: '您尚未登录请登录' }
         return
     }
+    console.log(`当前时间 ${Date.now()}: debug 的数据是 toUser: `, toUser)
+    console.log(`当前时间 ${Date.now()}: debug 的数据是 money: `, money)
     db[toUser] += (+money)
     db[id] -= (+money)
     ctx.body = { errno: 0 }
